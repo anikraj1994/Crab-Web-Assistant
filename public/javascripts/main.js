47a5b255eda41b38bc9223030353e5119fd35e59
Anik Raj <anikraj1994@gmail.com>
2017-03-19 17:08:42 +0530

    adding uber

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,135 +1,136 @@
 var recognizing = false;
 var ignore_onend;
 var start_timestamp;
-var final_span=document.getElementById("final_span");
-var interim_span=document.getElementById("interim_span");
+var final_span = document.getElementById("final_span");
+var interim_span = document.getElementById("interim_span");
 
 
-        if (!('webkitSpeechRecognition' in window)) {
-        addToList('<p>Speech Recognition not supported :(</p>',false);
-        } else {
+if (!('webkitSpeechRecognition' in window)) {
+    addToList('<p>Speech Recognition not supported :(</p>', false);
+} else {
 
-        if(sessionStorage.conversationId){
-            getSocketLink().then(function(){
-                connectToSocket();
-            });
-        }
-        else{
-            register().then(function(){
-                connectToSocket();
-            });
-        }
-        var recognition = new webkitSpeechRecognition();
-        recognition.continuous = false;
-        recognition.interimResults = true;
+    if (sessionStorage.conversationId) {
+        getSocketLink().then(function() {
+            connectToSocket();
+        });
+    } else {
+        register().then(function() {
+            connectToSocket();
+        });
+    }
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = false;
+    recognition.interimResults = true;
 
-        recognition.onstart = function() {
-            recognizing = true;
-            document.getElementById("fab-image").src="/images/mic-listening.png";
-            animate_fab_right();
-            console.log("start");
-        };
+    recognition.onstart = function() {
+        recognizing = true;
+        document.getElementById("fab-image").src = "/images/mic-listening.png";
+        animate_fab_right();
+        console.log("start");
+    };
 
-        recognition.onerror = function(event) {
-            alert(event.error+"");
-            console.log("error");
-            
-        };
+    recognition.onerror = function(event) {
+        alert(event.error + "");
+        console.log("error");
 
-        recognition.onend = function() {
-            recognizing = false;
-            document.getElementById("fab-image").src="/images/mic.png";  
-            animate_fab_left();    
-            console.log("end");
-                  
-        };
+    };
 
-        recognition.onresult = function(event) {
-            console.log("results");
-            
-            var interim_transcript = '';
-            for (var i = event.resultIndex; i < event.results.length; ++i) {
+    recognition.onend = function() {
+        recognizing = false;
+        document.getElementById("fab-image").src = "/images/mic.png";
+        animate_fab_left();
+        console.log("end");
+
+    };
+
+    recognition.onresult = function(event) {
+        console.log("results");
+
+        var interim_transcript = '';
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
             if (event.results[i].isFinal) {
                 final_transcript += event.results[i][0].transcript;
             } else {
                 interim_transcript += event.results[i][0].transcript;
             }
-            }
-            final_transcript = capitalize(final_transcript);
-            final_span.innerHTML = linebreak(final_transcript);
-            interim_span.innerHTML = linebreak(interim_transcript);
-
-            if(final_transcript){
-                //addToList('<p>'+final_transcript+'</p>',true);
-               // callWit(final_transcript);
-                sendToBot(final_transcript);
-            }
-        };
         }
-        var two_line = /\n\n/g;
-        var one_line = /\n/g;
-        function linebreak(s) {
-        return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
-        }
+        final_transcript = capitalize(final_transcript);
+        final_span.innerHTML = linebreak(final_transcript);
+        interim_span.innerHTML = linebreak(interim_transcript);
 
-        var first_char = /\S/;
-        function capitalize(s) {
-        return s.replace(first_char, function(m) { return m.toUpperCase(); });
+        if (final_transcript) {
+            //addToList('<p>'+final_transcript+'</p>',true);
+            // callWit(final_transcript);
+            sendToBot(final_transcript);
         }
+    };
+}
+var two_line = /\n\n/g;
+var one_line = /\n/g;
 
-        function startButton(event) {
-        if (recognizing) {
-            recognition.stop();
-            return;
+function linebreak(s) {
+    return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
+}
+
+var first_char = /\S/;
+
+function capitalize(s) {
+    return s.replace(first_char, function(m) { return m.toUpperCase(); });
+}
+
+function startButton(event) {
+    if (recognizing) {
+        recognition.stop();
+        return;
+    }
+    final_transcript = '';
+    recognition.lang = "en-IN";
+    recognition.start();
+    ignore_onend = false;
+    final_span.innerHTML = '';
+    interim_span.innerHTML = '';
+    start_timestamp = event.timeStamp | "null";
+}
+
+function addToList(message, isMe) {
+
+    var xli = document.createElement("li");
+    var xdiv = document.createElement("div");
+    if (isMe) xli.className = "self";
+    else xli.className = "other";
+    xdiv.className = "msg";
+    xdiv.innerHTML = message;
+    xli.appendChild(xdiv);
+    document.getElementById("chat-window").append(xli);
+    final_span.innerHTML = "";
+    interim_span.innerHTML = "";
+
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
+function callWit(message) {
+    ajax("/api/?message=" + message).then(function(result) {
+        addToList('<p>' + result + '</p>', false);
+    }).catch(function() {
+        // An error occurred
+        console.log("error");
+        addToList("<p>Something went wrong. I couldnt contact my server</p>", false);
+    });
+}
+
+function sendToBot(message) {
+    ajax("/send?conversationId=" + sessionStorage.conversationId + "&message=" + message).then(function(result) {
+        if (result !== 'ok') {
+            addToList("<p>Send failed. Resending.</p>");
         }
-        final_transcript = '';
-        recognition.lang = "en-IN";
-        recognition.start();
-        ignore_onend = false;
-        final_span.innerHTML = '';
-        interim_span.innerHTML = '';
-        start_timestamp = event.timeStamp;
-        }    
+    }).catch(function() {
+        // An error occurred
+        console.log("error");
+        addToList("<p>Something went wrong. I couldnt contact my server</p>", false);
+    });
+}
 
-        function addToList(message,isMe){
-           
-                var xli=document.createElement("li");
-                var xdiv=document.createElement("div");
-               if(isMe) xli.className="self";
-               else xli.className="other";
-                xdiv.className="msg";
-                xdiv.innerHTML=message;
-                xli.appendChild(xdiv);
-                document.getElementById("chat-window").append(xli);
-  final_span.innerHTML = "";
-            interim_span.innerHTML ="";
-            
-            
-        }
-
-        function callWit(message){
-            ajax("/api/?message="+message).then(function(result) {
-                addToList('<p>'+result+'</p>',false);
-            }).catch(function() {
-                // An error occurred
-                console.log("error");
-                addToList("<p>Something went wrong. I couldnt contact my server</p>",false);
-            });
-        }
-
-        function sendToBot(message){
-            ajax("/send?conversationId="+sessionStorage.conversationId+"&message="+message).then(function(result) {
-                if(result!=='ok'){
-                addToList("<p>Send failed. Resending.</p>");
-            }
-            }).catch(function() {
-                // An error occurred
-                console.log("error");
-                addToList("<p>Something went wrong. I couldnt contact my server</p>",false);
-            });
-        }
-
-        function ajax(url) {
+function ajax(url) {
     return new Promise(function(resolve, reject) {
         var xhr = new XMLHttpRequest();
         xhr.onload = function() {
@@ -141,101 +142,107 @@ var interim_span=document.getElementById("interim_span");
     });
 }
 
-        function animate_fab_right(){
-            document.getElementById("results").className="result z-2";
-        let flip = new FLIP({
+function animate_fab_right() {
+    document.getElementById("results").className = "result z-2";
+    let flip = new FLIP({
         element: document.getElementById("fab"),
         duration: 300
-        });
-        flip.first();
-        flip.last('fab-container-right');
-        flip.invert();
-        flip.play();
-       flip.removeClass("fab-container");
-        }
-        function animate_fab_left(){
-            document.getElementById("results").className="result";
-            
-        let flip = new FLIP({
+    });
+    flip.first();
+    flip.last('fab-container-right');
+    flip.invert();
+    flip.play();
+    flip.removeClass("fab-container");
+}
+
+function animate_fab_left() {
+    document.getElementById("results").className = "result";
+
+    let flip = new FLIP({
         element: document.getElementById("fab"),
         duration: 300
+    });
+    flip.first();
+    flip.last('fab-container');
+    flip.invert();
+    flip.play();
+    flip.removeClass("fab-container-right");
+
+}
+
+
+function connectToSocket() {
+    if ("WebSocket" in window) {
+        var ws = new WebSocket(sessionStorage.socketLink);
+
+        ws.onopen = function() {
+            addToList("<p>Connected</p>", false);
+            addToList("<p>Hello?</p>", false);
+            document.getElementById("fab").hidden = false;
+            // Web Socket is connected, send data using send()
+            //   ws.send("Message to send");
+            //   alert("Message is sent...");
+        };
+
+        ws.onmessage = function(evt) {
+            var received_msg = "";
+            try {
+                received_msg = JSON.parse(evt.data);
+            } catch (e) {
+                console.log(e + "");
+            }
+            if (received_msg.activities[0].from.id === 'crab') {
+                addToList('<p>' + received_msg.activities[0].text + '</p>', false);
+                // var tts = new GoogleTTS('en');
+                // tts.play(received_msg.activities[0].text);
+                responsiveVoice.speak(received_msg.activities[0].text, "UK English Male", {
+                    onend: function() {
+                        if (String(received_msg.activities[0].text).includes("?")) startButton(evt);
+                        else console.log("nq");
+                    }
+                });
+
+            } else
+                addToList('<p>' + received_msg.activities[0].text + '</p>', true);
+        };
+
+        ws.onclose = function() {
+            // websocket is closed.
+            addToList("<p>Disconnected</p>", false);
+        };
+    } else {
+        // The browser doesn't support WebSocket
+        addToList("<p>WebSocket NOT supported by your Browser!</p>");
+    }
+}
+
+
+//register
+function register() {
+    return new Promise(function(resolve, reject) {
+        ajax("/register").then(function(result) {
+            sessionStorage.conversationId = JSON.parse(result).conversationId;
+            sessionStorage.socketLink = JSON.parse(result).streamUrl;
+            resolve();
+        }).catch(function() {
+            // An error occurred
+            reject();
+            console.log("error");
+            addToList("<p>Something went wrong. I couldnt Register</p>", false);
         });
-        flip.first();
-        flip.last('fab-container');
-        flip.invert();
-        flip.play();
-       flip.removeClass("fab-container-right");
-        
-        }
-
-
-        function connectToSocket(){
-            if ("WebSocket" in window)
-            {
-               var ws = new WebSocket(sessionStorage.socketLink);
-				
-               ws.onopen = function()
-               {
-                   addToList("<p>Connected</p>",false);
-                   addToList("<p>Hello?</p>",false);
-                   document.getElementById("fab").hidden=false;
-                  // Web Socket is connected, send data using send()
-                //   ws.send("Message to send");
-                //   alert("Message is sent...");
-               };
-				
-               ws.onmessage = function (evt) 
-               { 
-                  var received_msg = JSON.parse(evt.data);
-                  if(received_msg.activities[0].from.id==='crab')
-                   addToList('<p>'+received_msg.activities[0].text+'</p>',false);
-                   else 
-                   addToList('<p>'+received_msg.activities[0].text+'</p>',true);
-               };
-				
-               ws.onclose = function()
-               { 
-                  // websocket is closed.
-                   addToList("<p>Disconnected</p>",false);
-               };
-            }
-            
-            else
-            {
-               // The browser doesn't support WebSocket
-               addToList("<p>WebSocket NOT supported by your Browser!</p>");
-            }
-        }
-
-
-        //register
-        function register(){
-             return new Promise(function(resolve, reject) {
-            ajax("/register").then(function(result) {
-                sessionStorage.conversationId=JSON.parse(result).conversationId;
-                sessionStorage.socketLink=JSON.parse(result).streamUrl;
-                resolve();
-            }).catch(function() {
-                // An error occurred
+    });
+}
+//getSocketLink
+function getSocketLink() {
+    return new Promise(function(resolve, reject) {
+        ajax("/getSocketLink?conversationId=" + sessionStorage.conversationId).then(function(result) {
+            sessionStorage.socketLink = JSON.parse(result).streamUrl;
+            resolve();
+        }).catch(function() {
+            // An error occurred
             reject();
-                console.log("error");
-                addToList("<p>Something went wrong. I couldnt Register</p>",false);
-            });
-             });
-        }
-        //getSocketLink
-        function getSocketLink(){
-             return new Promise(function(resolve, reject) {
-            ajax("/getSocketLink?conversationId="+sessionStorage.conversationId).then(function(result) {
-                sessionStorage.socketLink=JSON.parse(result).streamUrl;
-                resolve();
-            }).catch(function() {
-                // An error occurred
-            reject();
-                console.log("error");
-                addToList("<p>Something went wrong. I couldnt Register</p>",false);
-            });
-             });
-        }
-
-        
+            console.log("error");
+            addToList("<p>Something went wrong. I couldnt Register</p>", false);
+        });
+    });
+}
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,18 @@
 let Wit = null;
 var express = require('express');
 var router = express.Router();
+var Uber = require('node-uber');
+var uber = new Uber({
+    client_id: 'exGa_FaLJ7vjpXBVS81wb1js-YOH6Odx',
+    client_secret: 'gLnGeh3A7DGAO5-R6Gn9HyECWhT8299e3Km30VWG',
+    server_token: 'dPDUMh8K09edXz1vWG-43oR-LWEHTOMyKZGm7wqW',
+    redirect_uri: '/api/uberCallback',
+    name: 'Crab Assistant',
+    language: 'en_US', // optional, defaults to en_US
+    sandbox: true // optional, defaults to false
+});
+
+var Msession;
 // Wit = require('node-wit').Wit;
 
 var builder = require('botbuilder');
@@ -13,60 +25,110 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 var intents = new builder.IntentDialog();
 
-router.post('/bot',connector.listen());
+router.post('/bot', connector.listen());
+
+router.get('/uberLogin', function(request, response) {
+    var url = uber.getAuthorizeUrl(['history', 'profile', 'request', 'places']);
+    response.redirect(url);
+});
+
+router.get('/uberCallback', function(request, response) {
+    uber.authorizationAsync({ authorization_code: request.query.code })
+        .spread(function(access_token, refresh_token, authorizedScopes, tokenExpiration) {
+            // store the user id and associated access_token, refresh_token, scopes and token expiration date
+            console.log('New access_token retrieved: ' + access_token);
+            console.log('... token allows access to scopes: ' + authorizedScopes);
+            console.log('... token is valid until: ' + tokenExpiration);
+            console.log('... after token expiration, re-authorize using refresh_token: ' + refresh_token);
+
+            Msession.userData.uberAccessToken = access_token;
+            Msession.userData.uberAuthorizedScopes = authorizedScopes;
+            Msession.userData.uberTokenExpiration = tokenExpiration;
+            Msession.userData.uberRefresh_token = refresh_token;
+
+            // redirect the user back to your actual app
+            response.redirect('/');
+        })
+        .error(function(err) {
+            console.error(err);
+        });
+});
 
 bot.dialog('/', intents);
 
 intents.matches(/^change name/i, [
-    function (session) {
-      console.log("match change name");      
+    function(session) {
+        console.log("match change name");
         session.beginDialog('/profile');
     },
-    function (session, results) {
+    function(session, results) {
         session.send('Ok... Changed your name to %s', session.userData.name);
     }
 ]);
 
 intents.matches(/^what is the time/i, [
-    function (session) {
-      console.log("match time1");
-      session.beginDialog('/time');
+    function(session) {
+        console.log("match time1");
+        session.beginDialog('/time');
     }
 ]);
 intents.matches(/^time/i, [
-    function (session) {
-      console.log("match time2");
-      session.beginDialog('/time');
+    function(session) {
+        console.log("match time2");
+        session.beginDialog('/time');
+    }
+]);
+
+intents.matches(/^book cab/i, [
+    function(session) {
+        session.beginDialog('/uber');
+    }
+]);
+intents.matches(/^book uber/i, [
+    function(session) {
+        session.beginDialog('/uber');
     }
 ]);
 
 
 intents.onDefault([
-    function (session, args, next) {
+    function(session, args, next) {
         if (!session.userData.name) {
             session.beginDialog('/profile');
         } else {
             next();
         }
     },
-    function (session, results) {
+    function(session, results) {
         session.send('Hello %s!', session.userData.name);
     }
 ]);
 
 bot.dialog('/profile', [
-    function (session) {
+    function(session) {
         builder.Prompts.text(session, 'Hi! What is your name?');
     },
-    function (session, results) {
+    function(session, results) {
         session.userData.name = results.response;
         session.endDialog();
     }
 ]);
 bot.dialog('/time', [
-    function (session) {
-        session.send('The time is '+new Date().getHours()+":"+new Date().getMinutes()); 
-        session.endDialog();       
+    function(session) {
+        session.send('The time is ' + new Date().getHours() + ":" + new Date().getMinutes());
+        session.endDialog();
+    }
+]);
+bot.dialog('/uber', [
+    function(session) {
+        Msession = session;
+        if (!session.userData.uberAccessToken) {
+            router.redirect("/uberLogin");
+        } else {
+            console.log('access_token exist: ' + session.userData.uberAccessToken);
+        }
+        // session.send('The time is ' + new Date().getHours() + ":" + new Date().getMinutes());
+        session.endDialog();
     }
 ]);
 
